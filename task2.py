cd41f6e0b60fe84a22896ced0741eca5316c5009
svitlanachekan <spy1374@gmail.com>
2025-10-20 00:36:04 +0200

    homework

---
+++ task2.py
@@ -0,0 +1,30 @@
+# Вік через рік: Напишіть програму, яка запитує ім'я користувача та його вік. Потім виведіть повідомлення, яке повідомляє, скільки йому буде років через рік.
+# name = input("What is your name? ")
+# age = input('How old are you? ')
+# print(name + ', in 2026 you will be', int(age) + 1, 'years old.')
+
+# Площа прямокутника: Напишіть програму, яка запитує довжину та ширину прямокутника, а потім обчислює та виводить його площу.
+# length = input("What is the length of a rectangle? ")
+# width = input("What is the width of a rectangle? ")
+# s = int(length) * int(width)
+# print("The area of a rectangle is",s)
+
+# Конвертер валют: Запропонуйте користувачеві ввести суму в одній валюті (наприклад, доларах), а потім виведіть цю суму в іншій валюті (наприклад, євро), використовуючи заздалегідь визначений коефіцієнт обміну.
+# d = float(input("Please enter currency in dollar: "))
+# e = int(d) * 0.86
+# print("The amount in euro is", e, "€")
+
+# Вартість покупки: Запитайте користувача назву товару, його ціну та кількість. Обчисліть загальну вартість покупки та виведіть її на екран.
+# item = input("What is a name of the item? ")
+# price = input("What is its price? ")
+# quantity = input("How many is it there? ")
+# to_pay = int(price) * int(quantity)
+# print("The total purchase price is", to_pay, '€')
+
+# Температура: Напишіть програму, яка перетворює температуру з градусів Цельсія на Фаренгейт. Запитайте користувача температуру в градусах Цельсія.
+# temperature_in_celsius = float(input("What is the temperature in °C? "))
+# temperature_in_fahrenheit = (temperature_in_celsius * (9/5)) + 32
+# print("Temperature in fahrenheit is", temperature_in_fahrenheit, "°F")
+
+
+      
