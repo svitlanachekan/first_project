8c2957f969f3f0e966a77929f715c65d7bf2031a
svitlanachekan <spy1374@gmail.com>
2025-10-21 23:13:52 +0200

    homework_input_print

---
+++ homework_input_print.py
@@ -0,0 +1,51 @@
+# Задача 1. Середнє трьох чисел
+# Користувач вводить три числа. Обчисли середнє арифметичне.
+
+# n1 = int(input("Enter a number1: "))
+# n2 = int(input("Enter a number2: "))
+# n3 = int(input("Enter a number3: "))
+# print("Arithmetic average is" , int((n1+n2+n3) / 3))
+
+# Задача 2. Остача від ділення
+# Користувач вводить два числа. Знайди остачу від ділення першого на друге.
+
+# n4 = int(input("Enter a number4: "))
+# n5 = int(input("Enter a number5: "))
+# print("Remainder from division is", int(n4 % n5))
+
+# Задача 3. Подвоєне число
+# Користувач вводить число. Виведи подвоєне значення цього числа.
+
+# n6 = int(input("Enter a number6: "))
+# print("Doubling value is", int(n6 * 2))
+
+# Задача 4. Конвертація хвилин у секунди
+# Користувач вводить кількість хвилин. Обчисли, скільки це буде секунд.
+
+# n7 = int(input("Enter a number7: "))
+# print(n7, "minutes is", int(n7 * 60), "seconds")
+
+# Задача 5. Кількість яблук на кожного
+# Є n яблук і k учнів. Скільки яблук отримає кожен учень, якщо ділити порівну, і скільки залишиться?
+
+# n = int(input("Enter a number: "))
+# k = int(input("Enter a number: "))
+# print("Each pupil gets", int(n // k), "apple(s) and", int(n % k), "apple(s) is (are) left.")
+
+# Задача 6. Остання цифра числа
+# Користувач вводить число. Виведи його останню цифру.
+
+# n8 = int(input("Enter a number: "))
+# n9 = 10
+# print("Last number is",int(n8 % n9))
+
+# Задача 7*. Обмін змінних
+# Користувач вводить два числа. Після цього потрібно “поміняти” їх місцями і вивести результат.
+
+# n10 = int(input("Enter a number: "))
+# n11 = int(input("Enter a number: "))
+# a = n10
+# n10 = n11
+# n11 = a
+# print(n10, n11)
+
