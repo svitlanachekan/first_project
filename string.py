6c6fe6e61ccdeb710df60210c206b0327eedb5a1
svitlanachekan <spy1374@gmail.com>
2025-10-21 06:12:52 +0200

    selectively hw

---
+++ string.py
@@ -0,0 +1,28 @@
+# print("QWERTYdsd34".lower())
+# print("QWERTYdsd34".upper())
+# print("QWERTYdsd34".isdigit())
+
+# 1. Напишіть програму, яка приймає від користувача рядок, і відображає цей рядок у верхньому і нижньому регістрах.
+# s = input("Enter a string: ")
+# print(s.upper(), s.title())
+
+# 2. Скласти програму, яка запитує назву баскетбольної команди і повторює її на екрані зі словами: This is a champion!.
+# name = input("What a name of the basketball team? ")
+# print(name, "This is a champion!")
+
+# 3. Дано натуральне число. Знайти число, що отримується при прочитанні його цифр справа наліво.
+
+# n = input("Enter natural number mote than 0: ")
+# print("Reading numbers from riths to left: ", n[::-1])
+
+# Дано рядок. Визначити порядковий номер першої вказаної букви. Якщо такої літери немає, вивести нуль.
+
+# line = input("Enter a word: ")
+# letter = input("Enter a letter: ")
+# if letter in line:
+#     place = line.find(letter)+1
+#     print("Serial number: ", place)
+# else:
+#     print("0")
+
+
---
+++ task3.py
@@ -0,0 +1,21 @@
+# Ви придбали товар на певну суму s. Скільки купюр різного номіналу треба віддати продавцю, якщо починати платити з найбільших? У вас є 1, 2, 5, 10, 100, 500 гривень.
+# sum = float(input("Enter money: "))
+# money_500 = sum // 500
+# print("How many banknots of 500 hryvna: ", money_500)
+
+sum1 = int(input(""))
+money_100 = sum - money_500
+print(money_100)
+
+
+# money_10 = sum // 10
+# money_5 = sum // 5
+# money_2 = sum // 2
+# money_1 = sum // 1
+
+# sum_1 = sum % money_500
+# print(sum_1)
+
+
+#
+# print("How many banknots are needed to pay for goods: ", mo"items")
